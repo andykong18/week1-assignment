76defb7732aea6bbd7b75772bb70bbc38e056059
Andy <andykong0228@gmail.com>
2021-04-16 21:39:41 -0700

    Added foo to the bar

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,15 @@
 // Import HTMLElement here
-
+import HTMLElement from './HTMLElement';
 // Define class here
-
+class DivElement extends HTMLElement{
+    constructor(content){
+        super('div',content);
+        this.content = content;
+    }
+    render(){
+        const divElement = new HTMLElement('div', this.content); 
+        return divElement.render();
+    } 
+}
 // Export class here
+export default DivElement;
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,30 @@
-function Timer(seconds) {
-  this.seconds = seconds;
-}
-
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
+class Timer{
+  constructor(seconds){
+    this.seconds = seconds;
+  }
+  start(){
+    const timerInterval = setInterval(() => {
+      if (instance.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+  
+      console.log(instance.seconds);
+      instance.seconds -= 1;
+    }, 1000);
+  }
 };
 
 export default Timer;
+
+
+// Timer.prototype.start = function () {
+//   const instance = this;
+//   const timerInterval = setInterval(() => {
+//     if (instance.seconds === 0) {
+//       clearInterval(timerInterval);
+//     }
+
+//     console.log(instance.seconds);
+//     instance.seconds -= 1;
+//   }, 1000);
+// };
